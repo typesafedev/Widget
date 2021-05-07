55df6f80e820551f07cda11a3020154b4503161b
typesafedev <typesafedev@gmail.com>
2021-05-07 07:59:26 +0100

    Simplify Rectangle

--- src/Widget.App/Models/Textbox.cs
+++ src/Widget.App/Models/Textbox.cs
@@ -3,28 +3,15 @@ using Widget.App.Interfaces;
 
 namespace Widget.App.Models
 {
-  public record Textbox : Widget, ITextbox
+  public record Textbox : Rectangle, ITextbox
   {
-    private readonly int width;
-    private readonly int height;
-    public int Width
-    {
-      get => width;
-      init => width = (value < 0 ? throw new ArgumentOutOfRangeException($"{nameof(Width)} must be positive") : value);
-    }
-    public int Height
-    {
-      get => height;
-      init => height = (value < 0 ? throw new ArgumentOutOfRangeException($"{nameof(Height)} must be positive") : value);
-    }
-
     public string Text
     {
       get;
       init;
     }
 
-    public Textbox(int Width, int Height, string Text, int X, int Y) : base(X, Y)
+    public Textbox(int Width, int Height, string Text, int X, int Y) : base(X, Y, Width, Height)
     {
       this.Width = Width;
       this.Height = Height;
